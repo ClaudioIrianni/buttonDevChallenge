bbef39ead23fb9c6359813345ed1f4652b40ad5a
Claudio Alexis <irianniclaudio@gmail.com>
2022-09-01 15:27:38 -0300

    add css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-
+import Button from './components/Button'
+import button from './components/button.css'
 
 function App() {
   
-
+  
   return (
-   <>
+    <>
+    <h1>Buttons</h1>
    </>
   )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import button from './button.css'
+
+const Button = () => {
+  return (
+    <text>Default</text>
+  )
+}
+
+export default Button
